cba8b4a467075901d243c69753b64b4e26056dd0
Diana Vodopivec <dianavodopivec@MacBook-Air-de-Matias.local>
2023-12-05 14:48:53 -0300

    hepia3 - parte 1

--- scriptFetch.js
+++ scriptFetch.js
@@ -4,3 +4,29 @@ const $form = d.querySelector(".crud-form");
 const $title = d.querySelector(".crud-title");
 const $sendButton = d.getElementById("send");
 const $fragment = d.createDocumentFragment();
+
+const printer = (array) => {
+    array.forEach(character => {
+        console.log(character)
+    })
+}
+
+const consumeApi = async () => {
+    try {
+        const response = await fetch("http://localhost:5000/cyberpunk-characters")
+        const data = await response.json()
+        console.log(data)
+        //Aquí se manejan los ERRORES\\
+        if(!response.ok) {
+            throw {
+                message: response.statusText || "😢",
+                status: response.status
+            }
+        }
+        printer(data)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+consumeApi()
