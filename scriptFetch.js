1d58701d6ea52e3bfc97ad251bf011a76bd57590
Diana Vodopivec <dianavodopivec@MacBook-Air-de-Matias.local>
2023-12-05 12:59:05 -0300

    hepia2 - the end

---
+++ scriptFetch.js
@@ -0,0 +1,6 @@
+const d = document;
+const $table = d.querySelector(".content-characters");
+const $form = d.querySelector(".crud-form");
+const $title = d.querySelector(".crud-title");
+const $sendButton = d.getElementById("send");
+const $fragment = d.createDocumentFragment();
